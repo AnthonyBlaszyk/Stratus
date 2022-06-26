0f37b563d09c607d0a882e740189f1ab3d7e654f
Anthony <blaszyk.anthony@gmail.com>
2022-06-26 13:25:16 +0200

    feat: update text on homepage

--- utilities/categories.js
+++ utilities/categories.js
@@ -1,31 +1,31 @@
 export const categories = [
   {
     name: "Nos réalisations",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    desc: "Retrouvez ici toutes nos réalisation afin d'avoir un aperçu de nos compétences",
     color: "blue",
     slug: "real"
   },
   {
     name: "L'équipe",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    desc: "Une présentation rapide de notre grande équipe !",
     color: "green",
     slug: "team"
   },
   {
     name: "Notre façon de faire",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    desc: "Retrouvez ici une explications de notre méthodologie de travail",
     color: "yellow",
     slug: "workflow"
   },
   {
     name: "Tarif pack abonnement",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    desc: "Ici un aperçu de nos tarification et prix de nos packs et de nos propositions d'abonnement",
     color: "purple",
     slug: "subscription"
   },
   {
     name: "Contact",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    desc: "Besoin d'un devis ? Ou de nous faire part de votre projet ? Contactez nous ici !",
     color: "cyan",
     slug: "contact"
   },  
